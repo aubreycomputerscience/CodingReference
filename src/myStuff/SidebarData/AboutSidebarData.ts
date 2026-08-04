2ea179f26116672959cb844cfdf5dbe16548f5e8
Mr. C <aubreycomputerscience@gmail.com>
2026-08-03 23:58:39 -0500

    Add content

---
+++ src/myStuff/SidebarData/AboutSidebarData.ts
@@ -0,0 +1,25 @@
+import * as DataTypes from "../myDataTypes";
+
+export const data: DataTypes.SidebarGroupings = {  // create an object literal (kind of like JSON)
+
+    sidebarContentType: "About",  // we use a colon, not an =, and a comma at the end
+    sidebarGroups: [
+
+        {   displayName: DataTypes.SectionNames.About_Standards,
+            sidebarItems: [
+                { 
+                    displayName: "Year I",
+                    pageName: "cs1"
+                },
+                { 
+                    displayName: "Year II",
+                    pageName: "cs2"
+                },
+                                { 
+                    displayName: "Year III",
+                    pageName: "cs3"
+                },
+        ] },
+
+    ]
+}
